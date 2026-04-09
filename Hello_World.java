bd73562f482918c758bcaeeca0ea74c99855eefe
samecabh <samecabrahao@gmail.com>
2026-04-09 19:43:11 -0300

    Add files via upload
    O básico "Hello World".

---
+++ Hello_World.java
@@ -0,0 +1,10 @@
+public class Hello_World {
+
+    public static void main(String[] args) {
+        
+        // Exibir Mensagem
+        System.out.println("Hello, World!");
+        System.out.println("My name is Sâmec.");
+    }
+    
+}
