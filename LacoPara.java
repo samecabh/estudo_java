6598dd3e6545c8cd11d70bd3002a7d21349822f0
Sâmec Abrahão <samecabrahao@gmail.com>
2026-04-21 20:17:45 -0300

    Add files via upload
    Usando o laço para (for).

---
+++ LacoPara.java
@@ -0,0 +1,10 @@
+public class LacoPara {
+
+    public static void main(String[] args) {
+        
+        for(int indice = 1; indice <= 20; indice++){
+            System.out.println(indice);
+        }
+    }
+    
+}
