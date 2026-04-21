3b3854491f5d5eb17b91854114ef4e389dfc045d
Sâmec Abrahão <samecabrahao@gmail.com>
2026-04-21 20:09:52 -0300

    Add files via upload
    Usando o laço enquanto (while)

---
+++ LacoEnquanto.java
@@ -0,0 +1,13 @@
+public class LacoEnquanto {
+
+    public static void main(String[] args) {
+        
+        int indice = 1;
+
+        while(indice <= 10){
+            System.out.println(indice);
+            indice++;
+        }
+    }
+    
+}
