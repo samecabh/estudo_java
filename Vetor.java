7d1449292b4f257d202dd49301fb75963a4362ec
Sâmec Abrahão <samecabrahao@gmail.com>
2026-04-21 20:23:43 -0300

    Add files via upload

---
+++ Vetor.java
@@ -0,0 +1,14 @@
+public class Vetor {
+
+    public static void main(String[] args) {
+        
+        String[] nomes = {"Sâmec", "Abrahão", "Souza", "Lima"};
+
+        // Exibição de Dados
+        //System.out.println(nomes[0]);
+        for(int indice=0; indice <4; indice++){
+            System.out.println(nomes[indice]);
+        }
+    }
+    
+}
