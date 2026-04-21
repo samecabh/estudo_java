ce3e3979c9231ec83010ea9e58dd497407105181
Sâmec Abrahão <samecabrahao@gmail.com>
2026-04-21 20:27:32 -0300

    Add files via upload
    Usando o laço para cada (ForEach)

---
+++ LacoParaCada.java
@@ -0,0 +1,15 @@
+public class LacoParaCada {
+
+    public static void main(String[] args) {
+        
+        // Vetor
+        String[] cidades = {"São Paulo", "Hortolândia", "Sumaré", "Campinas"};
+
+        // Exibir
+        for(String localidade : cidades){
+            System.out.println(localidade);
+
+        }
+    }
+    
+}
